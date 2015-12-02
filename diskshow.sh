0ee41161e606ae16b6b670640d7e3bfd026c57be
guobao.jiang <loveaborn@foxmail.com>
2015-12-02 10:47:09 +0800

    update

--- diskshow.sh
+++ diskshow.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
+# nohup ./diskshow.sh >> log/disk.txt 2>&1&
+
 while :
 do
 	echo "--------------------------------------------"
 	echo `date +%Y-%m-%d-%H:%M.%S`
-	echo "df -h"
+	df -h
 	sleep 300
 done
