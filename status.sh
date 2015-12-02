77c535cc652ab12e0989d0bedfd0cfbfadc678a1
guobao.jiang <loveaborn@foxmail.com>
2015-12-02 14:07:28 +0800

    update

--- status.sh
+++ status.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # nohup ./status.sh > log/status.txt 2>&1& echo $! > status.pid
+# nohup ./status.sh 2>&1& echo $! > status.pid
 while :
 do
-    cat log/ci.txt |egrep ";;;|2015-12" |grep -v popkit
+    cat log/ci.txt |egrep ";;;|2015-12" |grep -v popkit > log/status.txt
     sleep 60
 done
 
