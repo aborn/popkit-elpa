491861b0074e68b0931747228c6fd5df99233a12
蒋国宝 <loveaborn@foxmail.com>
2016-02-04 00:37:20 +0800

    update

--- sync-melpa.sh
+++ sync-melpa.sh
@@ -4,5 +4,5 @@ cd /home/popkit/melpa
 git pull
 cp recipes/* /home/popkit/popkit-elpa/recipes
 cd /home/popkit/popkit-elpa
-git commit -am "update"
+git commit -am "sync melpa.org recipes"
 git push
