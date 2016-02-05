554ed00a2b8f17851a6c492098d6fc287c53df6a
Aborn Jiang <loveaborn@foxmail.com>
2016-02-05 14:43:04 +0800

    sync melpa.org recipes

--- sync-melpa.sh
+++ sync-melpa.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+echo `date +%Y-%m-%d-%H:%M.%S`
 cd /home/popkit/melpa
 git pull
 cp recipes/* /home/popkit/popkit-elpa/recipes
 cd /home/popkit/popkit-elpa
 git add -A .
-git commit -am "sync melpa.org recipes"
+git commit -am "sync melpa.org recipes date +%Y-%m-%d-%H:%M.%S"
 git push
